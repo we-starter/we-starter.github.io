0a79bbd7d35f9f76ab7faefce5fd97dfc009d2c6
jihuanyu <ji2240224966@163.com>
2021-01-26 21:33:06 +0800

    'Style'

--- src/pages/pools/stakingPool1.js
+++ src/pages/pools/stakingPool1.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ReactComponent as HUSD } from '../../assets/logo/HUSD.svg';
+import { ReactComponent as ONE } from '../../assets/icon/1X.svg';
 import { StakingItem } from './StakingItem';
 import ant from '../../assets/icon/ant.png';
 
@@ -12,7 +13,7 @@ export const StakingPool1 = () => {
             address: null,
             stakingAddress: '0xB65853Ddc2366564e2238c70a0676B886c79dD9b',
             logo: <HUSD />,
-            multiple: '1X',
+            multiple: <ONE />,
         },
         {
             id: 0,
@@ -21,7 +22,7 @@ export const StakingPool1 = () => {
             address: '0xad6d458402f60fd3bd25163575031acdce07538d',
             stakingAddress: '0xE9bA85Ef193c02a5583599676d93b408E106b60B',
             logo: <HUSD />,
-            multiple: '1X',
+            multiple: <ONE />,
         },
         {
             id: 0,
@@ -30,7 +31,7 @@ export const StakingPool1 = () => {
             address: '0x880bd31775d97Ce7006D1Cc72EbCC36E412E663C',
             stakingAddress: '0xDdB7B0a03A98e7814430E8C010D221D010F2cD6F',
             logo: <HUSD />,
-            multiple: '1X',
+            multiple: <ONE />,
         },
     ];
 
