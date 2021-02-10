020b20d82e61a65d4a2ff848ceb8f5ddf91b3465
sunxuan <1406243464@qq.com>
2021-02-10 23:03:21 +0800

    join弹框

--- src/components/staterPools/poolsJoin.js
+++ src/components/staterPools/poolsJoin.js
@@ -1,16 +1,61 @@
 import React, { useState } from 'react'
 import { usage } from 'browserslist'
+import { formatAmount } from '../../utils/format'
+import { GLFIcon } from '../../icons'
+import { Select } from 'antd'
 
-export const PoolsJoin = ({ onChange, onClose, onConfirm }) => {
-  const [amount, setAmount] = useState('')
-  const onMax = () => {}
+const { Option } = Select
+
+export const PoolsJoin = ({
+  amount,
+  onChange,
+  onMax,
+  icon,
+  balance,
+  onClose,
+  onConfirm,
+  symbol,
+}) => {
+  const handleChange = (value) => {
+    console.log(`selected ${value}`)
+  }
   return (
     <div className='modal'>
       <div className='modal__box'>
         <form className='form-app' action='/'>
           <div className='form-app__inner deposit'>
-            <h1 className='form-app__title h3'>Join XXX Pool</h1>
-
+            <h1
+              className='form-app__title h3'
+              style={{ marginTop: 0, marginBottom: '20px' }}
+            >
+              Join XXX Pool
+            </h1>
+            <p className='form-app__tip'>1HT = 143.22 WAR</p>
+            <Select
+              defaultValue='lucy'
+              style={{ width: 120 }}
+              onChange={handleChange}
+            >
+              <Option value='jack'>
+                {/* <GLFIcon width={24} height={24} />
+                {icon} */}
+                Jack
+              </Option>
+              <Option value='lucy'>Lucy</Option>
+              <Option value='Yiminghe'>yiminghe</Option>
+            </Select>
+            <p
+              className='form-app__inputbox-after-text'
+              style={{
+                marginBottom: 0,
+                color: '#22292F',
+                textAlign: 'left',
+                opacity: 1,
+              }}
+            >
+              Balance:{' '}
+              {balance && symbol && `${formatAmount(balance)} ${symbol}`} <br />
+            </p>
             <div className='deposit__inputbox form-app__inputbox'>
               <div className='form-app__inputbox-control'>
                 <div className='form-app__inputbox-input'>
