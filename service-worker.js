044f93cdcacb17b5b670f85f72ecdb0f2ce4e92c
SunDaYan <westarter@protonmail.com>
2021-03-18 15:47:45 +0000

    SunDaYan published a site update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.056e82f2e7d7e888d146c6197d56632d.js"
+  "/precache-manifest.10183acdcba1dfa56b02c60a6c21b54c.js"
 );
 
 self.addEventListener('message', (event) => {
