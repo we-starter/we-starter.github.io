0f9627275e3e76132de42621a3434d446d352f2f
sunxuan <1406243464@qq.com>
2021-04-09 16:20:35 +0800

    banner

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,6 +1,9 @@
 import React, { useContext, useState } from 'react'
 import { Link, NavLink, useLocation } from 'react-router-dom'
 import { FormattedMessage } from 'react-intl'
+import Icon1 from '../../assets/icon/icon1@2x.png'
+import Icon2 from '../../assets/icon/icon2@2x.png'
+import Icon3 from '../../assets/icon/icon3@2x.png'
 import WeStarterPDF from '../../pdfFile/Security Assessment for WeStarter - Starter.pdf'
 
 export const Banner = () => {
@@ -40,6 +43,29 @@ export const Banner = () => {
           href={WeStarterPDF}
           target='_blank'
         ></a> */}
+      </div>
+      <div className='banner_related'>
+        <div className='banner_related_data'>
+          <img src={Icon1} />
+          <p>
+            <span className='banner_related_data_title'>累计融资金额</span>
+            <span className='banner_related_data_val'>9,999,999.99 USD</span>
+          </p>
+        </div>
+        <div className='banner_related_data'>
+          <img src={Icon2} />
+          <p>
+            <span className='banner_related_data_title'>参与地址总数</span>
+            <span className='banner_related_data_val'>9,999,999.99</span>
+          </p>
+        </div>
+        <div className='banner_related_data'>
+          <img src={Icon3} />
+          <p>
+            <span className='banner_related_data_title'>总成立的池子数</span>
+            <span className='banner_related_data_val'>9,999,999.99</span>
+          </p>
+        </div>
       </div>
     </div>
   )
