7b9df8879198d406515cf1d066f6bdfebe5906e5
SunDaYan <westarter@protonmail.com>
2021-04-09 10:22:01 +0000

    SunDaYan published a site update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.631d5d90d7a62a942aaf1a311cbb9228.js"
+  "/precache-manifest.2f18f738fca4ea08941310c701ecff10.js"
 );
 
 self.addEventListener('message', (event) => {
---
+++ static/js/4.c76143c8.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgallery.finance.front"]=this["webpackJsonpgallery.finance.front"]||[]).push([[4],{1335:function(n,a){},1433:function(n,a){}}]);
+//# sourceMappingURL=4.c76143c8.chunk.js.map
