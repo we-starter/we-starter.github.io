f3e1d43471bf660792dfcc9bc1063101d502c09f
xubowenshizi <xubowenshizi@gmail.com>
2021-05-08 19:22:04 +0800

    update pool

--- src/configs/farm.js
+++ src/configs/farm.js
@@ -6,10 +6,10 @@ export default [
     name: 'WAR-HT MLP',
     rewards1: 'WAR',
     rewards2: 'MDX',
-    address: '0x2B8ab0f074114CC570626A1140219B4A4453d90b', // 超募合约地址
-    rewards1Address: '0x948d2a81086a075b3130bac19e4c6dee1d2e3fe8',
-    rewards2Address: '0x67ee3cb086f8a16f34bee3ca72fad36f7db929e2',
-    MLP: '0x7616792426A33728F58c63754eE073e05F773542',
+    address: ' 0xEEC6169616d6E50560E52460A2C74f74Cdf57b6F', // 超募合约地址
+    rewards1Address: '0x910651F81a605a6Ef35d05527d24A72fecef8bF0',
+    rewards2Address: '0x25D2e80cB6B86881Fd7e07dd263Fb79f4AbE033c',
+    MLP: '0xe4e55c9203ac398a0f0b98bd096b70d9778eca6a',
     abi: StakingPool,
     start_at: '1620482400',
     time: '1620655200',
