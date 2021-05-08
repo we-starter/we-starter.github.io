de1d990b4da8f0c7b534b3cf9a23e029a5b08dd6
xubowenshizi <xubowenshizi@gmail.com>
2021-05-08 19:26:42 +0800

    update conf

--- src/configs/farm.js
+++ src/configs/farm.js
@@ -6,7 +6,7 @@ export default [
     name: 'WAR-HT MLP',
     rewards1: 'WAR',
     rewards2: 'MDX',
-    address: ' 0xEEC6169616d6E50560E52460A2C74f74Cdf57b6F', // 超募合约地址
+    address: '0xEEC6169616d6E50560E52460A2C74f74Cdf57b6F', // 超募合约地址
     rewards1Address: '0x910651F81a605a6Ef35d05527d24A72fecef8bF0',
     rewards2Address: '0x25D2e80cB6B86881Fd7e07dd263Fb79f4AbE033c',
     MLP: '0xe4e55c9203ac398a0f0b98bd096b70d9778eca6a',
