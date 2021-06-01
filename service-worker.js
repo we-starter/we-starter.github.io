4563b4d3c651c9ee3af2255f5e304e2c0fb8fa11
westarter <westarter@protonmail.com>
2021-06-01 08:51:36 +0000

    Deploying to gh-pages from @ we-starter/we-starter.github.io@4912831c9a2ffc0cd1f82012391b9018e28c7075 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8a7421f02e756cbea931b3163de8eb1c.js"
+  "/precache-manifest.ca7f363882a775faf7b3d556c95c6cc0.js"
 );
 
 self.addEventListener('message', (event) => {
---
+++ static/js/4.cddd8f27.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgallery.finance.front"]=this["webpackJsonpgallery.finance.front"]||[]).push([[4],{1509:function(n,a){},1607:function(n,a){}}]);
+//# sourceMappingURL=4.cddd8f27.chunk.js.map
