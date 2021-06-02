4d9cd2877fb973937497c3ed6a96e45f7cbc9681
westarter <westarter@protonmail.com>
2021-06-02 09:33:40 +0000

    Deploying to gh-pages from @ we-starter/we-starter.github.io@0a180011266a816dc2a17ba74f1a3c0b5efce706 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6e986b1fd128bd44c28b70ed3f74daa2.js"
+  "/precache-manifest.2fd516f119ec1ea0c580abade83fbac6.js"
 );
 
 self.addEventListener('message', (event) => {
