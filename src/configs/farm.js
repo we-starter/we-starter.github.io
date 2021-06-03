26cd216289a63dfc8c6aa12a3c297a5fc19a0b47
xubowenshizi <xubowenshizi@gmail.com>
2021-06-03 14:56:58 +0800

    add conf

--- src/configs/farm.js
+++ src/configs/farm.js
@@ -66,4 +66,35 @@ export default [
     is_coming: false,
     mdexReward: false,
   },
+  {
+    name: 'WAR',
+    icon: 'PAUL-HT_small@2x.png',
+    rewards1: 'WAR',
+    rewards2: null,
+    address: '0x5b0F4cb9041cED035Fd0a7Db3c0C2f7f4dC62A66',
+    rewards1Address: '0x910651F81a605a6Ef35d05527d24A72fecef8bF0',
+    rewards2Address: null,
+    // apr
+    valueAprToken: "0x910651F81a605a6Ef35d05527d24A72fecef8bF0", // WAR
+    valueAprPath: [
+      "0x5545153CCFcA01fbd7Dd11C0b23ba694D9509A6F", // WHT
+    ],
+    rewardsAprPath: [
+      "0x5545153CCFcA01fbd7Dd11C0b23ba694D9509A6F", // WHT
+    ],
+    settleToken: '0xa71EdC38d189767582C38A3145b5873052c3e47a', //usdt
+    MLP: '0x910651F81a605a6Ef35d05527d24A72fecef8bF0', // WAR
+    byLink: 'https://ht.mdex.com/#/swap?outputCurrency=0x910651f81a605a6ef35d05527d24a72fecef8bf0',
+    abi: StakingPool,
+    start_at: '',
+    time: '',
+    openDate: null,
+    dueDate: null,
+    earnName: 'APY',
+    status: 0,
+    rewards: 'LPT',
+    decimal: 18,
+    is_coming: false,
+    mdexReward: false,
+  },
 ]
