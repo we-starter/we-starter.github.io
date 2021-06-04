59821eacb7cf981ccaeac702676d2edb4f8e490a
xubowenshizi <xubowenshizi@gmail.com>
2021-06-04 14:11:04 +0800

    farm sort

--- src/configs/farm.js
+++ src/configs/farm.js
@@ -37,38 +37,6 @@ export default [
     mdexReward: true,
   },
   {
-    name: 'PAUL-USDT LPT',
-    icon: 'PAUL-HT_small@2x.png',
-    rewards1: 'WAR',
-    rewards2: null,
-    address: '0x5b0F4cb9041cED035Fd0a7Db3c0C2f7f4dC62A66',
-    rewards1Address: '0x910651F81a605a6Ef35d05527d24A72fecef8bF0',
-    rewards2Address: null,
-    // apr
-    valueAprToken: '0xFC01b8f883a89278235ba674bbE2bb48db96d9Cf', // FAUL
-    valueAprPath: [],
-    rewardsAprPath: [
-      '0x5545153CCFcA01fbd7Dd11C0b23ba694D9509A6F', // WHT
-    ],
-    settleToken: '0xa71EdC38d189767582C38A3145b5873052c3e47a', //usdt
-    MLP: '0xD9baBF51f327829264f554B4Fa4e12Cec5BD0F50',
-    byLink:
-      'https://ht.mdex.com/#/add/0xa71EdC38d189767582C38A3145b5873052c3e47a/0xFC01b8f883a89278235ba674bbE2bb48db96d9Cf',
-    abi: StakingPool,
-    start_at: '',
-    time: '',
-    // dueDate: getRemainTime('2021/06/07 18:00 UTC+8'),
-    // openDate: getRemainTime('2021/06/01 18:00 UTC+8'),
-    openDate: '1622541600',
-    dueDate: '1625133600',
-    earnName: 'APR',
-    status: 0,
-    rewards: 'LPT',
-    decimal: 18,
-    is_coming: false,
-    mdexReward: false,
-  },
-  {
     name: 'WAR',
     icon: 'PAUL-HT_small@2x.png',
     rewards1: 'WAR',
@@ -96,6 +64,38 @@ export default [
     earnName: 'APY',
     status: 0,
     rewards: 'WAR',
+    decimal: 18,
+    is_coming: false,
+    mdexReward: false,
+  },
+  {
+    name: 'PAUL-USDT LPT',
+    icon: 'PAUL-HT_small@2x.png',
+    rewards1: 'WAR',
+    rewards2: null,
+    address: '0x5b0F4cb9041cED035Fd0a7Db3c0C2f7f4dC62A66',
+    rewards1Address: '0x910651F81a605a6Ef35d05527d24A72fecef8bF0',
+    rewards2Address: null,
+    // apr
+    valueAprToken: '0xFC01b8f883a89278235ba674bbE2bb48db96d9Cf', // FAUL
+    valueAprPath: [],
+    rewardsAprPath: [
+      '0x5545153CCFcA01fbd7Dd11C0b23ba694D9509A6F', // WHT
+    ],
+    settleToken: '0xa71EdC38d189767582C38A3145b5873052c3e47a', //usdt
+    MLP: '0xD9baBF51f327829264f554B4Fa4e12Cec5BD0F50',
+    byLink:
+      'https://ht.mdex.com/#/add/0xa71EdC38d189767582C38A3145b5873052c3e47a/0xFC01b8f883a89278235ba674bbE2bb48db96d9Cf',
+    abi: StakingPool,
+    start_at: '',
+    time: '',
+    // dueDate: getRemainTime('2021/06/07 18:00 UTC+8'),
+    // openDate: getRemainTime('2021/06/01 18:00 UTC+8'),
+    openDate: '1622541600',
+    dueDate: '1625133600',
+    earnName: 'APR',
+    status: 0,
+    rewards: 'LPT',
     decimal: 18,
     is_coming: false,
     mdexReward: false,
