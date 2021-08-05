e74f7c4cb07d150320fd3cae39cd39c181f24431
ydev <ydev@gmail.com>
2021-08-05 14:27:14 +0800

    hide migrate

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -95,17 +95,17 @@ export const Header = () => {
                     <span className='menu__hot'></span>
                   </NavLink>
                 </li>
-                {active && (
-                  <li className='menu__item'>
-                    <NavLink
-                      to='/migrate'
-                      className='menu__link'
-                      onClick={handleMenuItemClick}
-                    >
-                      <FormattedMessage id='bridge' />
-                    </NavLink>
-                  </li>
-                )}
+                {/*{active && (*/}
+                {/*  <li className='menu__item'>*/}
+                {/*    <NavLink*/}
+                {/*      to='/migrate'*/}
+                {/*      className='menu__link'*/}
+                {/*      onClick={handleMenuItemClick}*/}
+                {/*    >*/}
+                {/*      <FormattedMessage id='bridge' />*/}
+                {/*    </NavLink>*/}
+                {/*  </li>*/}
+                {/*)}*/}
                 <li className='menu__item'>
                   <a
                     href='https://forms.gle/n6M5tJ46KtEfEDQz9'
