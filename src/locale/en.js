43c89d7122dc857d3f4fd5adbdad5b101d6a8cd5
ydev <ydev@gmail.com>
2021-11-21 16:08:04 +0800

    rule txt

--- src/locale/en.js
+++ src/locale/en.js
@@ -442,7 +442,7 @@ const en_US = {
   applicationText37:
     'If the proposal is within the cycle and equals or exceeds 50% of the total IWO of the project, it is a success, and the voter and the project sponsor (proposer) can claim back their WAR after 5 days',
   applicationText38:
-    'If the proposal is less than 50% of the total IWO quota of the project party within the period, it is a failure. 50% of the pledge amount of the proposal will be airdropped to the voting parties, and the remaining 50% of the pledge amount will be burned. Voting parties can claim back their WAR after 5 days.',
+    'If the proposal is less than 50% of the total IWO quota of the project party within the period, it is a failure. 50% of the pledge amount of the proposal will be airdropped to the voting parties. Voters and project sponsors (proposers) can claim back their WAR after 5 days.',
   applicationText39:
     'The NFT Card contains the official project info provided by the project team, then WeStarter casts the information into NFT. All the information cannot be changed if the card finished.',
 
